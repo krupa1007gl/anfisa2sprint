42cc95516ed0ae4ec18202804d75ee79e9a7774a
krupa1007gl <krupa.xleb@gmail.com>
2026-03-04 14:37:05 +0300

    anfisa2

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,8 +1,43 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
 from .models import Category, IceCream, Topping, Wrapper
 
+empty_value_display = 'Не задано'
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+    IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+class IceCreamAdmin(admin.ModelAdmin):
+    filter_horizontal = ('toppings',)
+
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Topping)
 admin.site.register(Wrapper)
-admin.site.register(IceCream)
-admin.site.register(Category)
